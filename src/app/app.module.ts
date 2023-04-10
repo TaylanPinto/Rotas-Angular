e8d5bbb5066b21daf911ba1aea5d7aba8105ce20
taylan <taylanlaflamme@gmail.com>
2023-04-10 10:51:24 -0300

    Adicionando arquivos de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { NgModule } from '@angular/core';
+import { CursosModule } from './../../../servicos/src/app/cursos/cursos.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+
+import { HomeComponent } from './home/home.component';
+//import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+//import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+//import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 import { AppComponent } from './app.component';
+import { routing } from './app.routing';
+//import { CursosService } from './cursos/cursos.service';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent/*,
+    CursosComponent,
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent*/
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    routing,
+    CursosModule
   ],
-  providers: [],
+  //providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+  
